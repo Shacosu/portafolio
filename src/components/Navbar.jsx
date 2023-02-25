01b804634a570bcf0ebe36b195bd4a8b3f14fb69
Shacosu <sh4c0p@gmail.com>
2023-02-24 22:28:17 -0300

    Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
 	return (
 		<nav className="h-24 flex justify-between items-center container mx-auto py-4 ">
-			<div className="flex items-center gap-3 mx-auto lg:mx-1">
+			<div className="flex items-center gap-3 ">
 				<img
 					src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
 					alt=""
