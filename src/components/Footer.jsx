7c38497859f6cc7bcdfe1a9e9267219a801a9e41
Shacosu <sh4c0p@gmail.com>
2023-06-30 03:31:35 -0400

    calendly popup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="h-20 grid grid-cols-12 place-items-center mt-8 bg-gradient-to-r from-gray-800 via-gray-600 to-gray-800">
+    <footer className="h-20 grid grid-cols-12 place-items-center mt-8 bg-gray-900">
         <div className="col-span-4">
-            Footer start
+            
         </div>
         <div className="col-span-4">
             Pagina creada por Pablo Espinoza &copy; Copyright { new Date().getFullYear() }
         </div>
         <div className="col-span-4">
-            Footer end
+            
         </div>
     </footer>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+
 export default function Navbar() {
+	useEffect(() => {
+		const nav = document.querySelector("nav");
+		const sticky = nav.offsetTop;
+		
+		const scrollCallBack = window.addEventListener("scroll", () => {
+			if (window.scrollY > 100) {
+				nav.classList.add("bg-gray-900");
+			} else if (window.scrollY < 100) {
+				nav.classList.remove("bg-gray-900");
+			}
+		});
+		return () => {
+			window.removeEventListener("scroll", scrollCallBack);
+		};
+	}, []);
 	return (
-		<nav className="h-24 flex justify-between items-center container mx-auto py-4 ">
+		<nav className="h-20 flex justify-around items-center py-4  transition duration-300 z-20">
 			<div className="flex items-center gap-3 ">
 				<img
 					src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
